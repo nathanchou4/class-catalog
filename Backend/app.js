3f0d9d59d299c411257b94bd43ec4ab76a28d3d1
Nathan Chou <nathanchou4@gmail.com>
2019-08-06 23:08:25 -0700

    Changed server.js to app.js for readability

---
+++ Backend/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var apiRoutes = require('./api-routes');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/admin', { useNewUrlParser: true });
+var db = mongoose.connection;
+if (!db) {c
+    console.log('error connecting to db');
+} else {
+    console.log('success connecting to db');
+}
+
+app.use('/', apiRoutes);
+
+var port = process.env.PORT || 8080;
+app.listen(port, () => console.log('listening to port ' + port));
